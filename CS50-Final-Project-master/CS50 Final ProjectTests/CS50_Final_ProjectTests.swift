a15d76f7720e80591c231bd8a74a597b3432ff25
MoritzBonacker <43189933+MoritzBonacker@users.noreply.github.com>
2018-11-26 20:15:45 -0500

    Add files via upload

---
+++ CS50-Final-Project-master/CS50 Final ProjectTests/CS50_Final_ProjectTests.swift
@@ -0,0 +1,34 @@
+//
+//  CS50_Final_ProjectTests.swift
+//  CS50 Final ProjectTests
+//
+//  Created by Moritz Bonacker on 13.11.18.
+//  Copyright © 2018 CS50 Project Team. All rights reserved.
+//
+
+import XCTest
+@testable import CS50_Final_Project
+
+class CS50_Final_ProjectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
